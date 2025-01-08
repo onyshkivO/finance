04869ca1abe62744b0d79e0812a0a8545e9b2433
Ostapko58 <prostoostap58@gmail.com>
2025-01-08 22:17:45 +0200

    initial commit

---
+++ backend/src/test/java/com/onyshkiv/finance/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onyshkiv.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
